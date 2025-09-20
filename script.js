793d3a1f8f1fd54243eba922b53a71f59d781357
finvest2005 <finvest2005@yandex.ru>
2025-09-20 23:40:55 +0300

    Implement getComputerChoice func for random choice rock, paper or scissors
    Test it by console.log in cycle from 1 to 10

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello there');
+function getComputerChoice() {
+  const randomFromZeroToOne = Math.random();
+  const randomFromOneToThree = Math.floor(randomFromZeroToOne * 3) + 1;
+  switch (randomFromOneToThree) {
+    case 1:
+      return 'rock';
+      break;
+    case 2:
+      return 'paper';
+      break;
+    case 3:
+      return 'scissors';
+      break;
+    default:
+      return undefined;
+  }
+}
+
+// test output for getComputerChoice() function
+for (let index = 1; index <= 10; index++) console.log(getComputerChoice());
